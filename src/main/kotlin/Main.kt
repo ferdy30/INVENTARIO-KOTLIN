f8b6fc39f85755cb0d8a49d143d8317caec3fbb5
fer <lovojuarez10@gmail.com>
2022-09-11 11:49:24 -0600

    Rama principal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,20 @@
 fun main(parametro: Array<String>) {
+
+
     println(".....................................................")
     println(".... BIENVENIDO AL SISTEMA DE FARMACIA FIORELLA.....")
     println("........... FAVOR INGRESAR SUS CREDENCIALES ..........")
     println(".....................................................")
 
+//INICIO DE LOGIN
 
     val empleado = usuario()
 
     empleado.validacion()
     empleado.continuar()
+
+
+
 
 
 }
--- src/main/kotlin/usuario.kt
+++ src/main/kotlin/usuario.kt
@@ -1,6 +1,6 @@
 class usuario(private var usuario: String, private var contrasena: String) {
     var op: Int? = null
-
+    val menu = Menu();
     constructor() : this("", "") {
 
         print("Ingrese su usuario:")
@@ -14,14 +14,17 @@ class usuario(private var usuario: String, private var contrasena: String) {
 
 
         if (usuario == "admin" && contrasena == "admin123") {
-            print("Usuario correcto.Bienvenido Admin")
+            print("Usuario correcto.Bienvenido Admin " )
+            print(menu.menu());
 
         } else if (usuario == "vendedor" && contrasena == "vendedor123") {
             println("usuario correcto.Bienvenido vendedor")
+            print(menu.menu());
 
 
         } else if (usuario == "invitado" && contrasena == "invitado123") {
             println("usuario correcto.Bienvenido Invitado")
+            print(menu.menu());
         } else {
 
             println("usuario incorrecto")
